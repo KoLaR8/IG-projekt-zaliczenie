38076b6f91287aebd322e29c813f411c6dd4ebdc
Wojtek Kolarczyk <wkolarczyk99@gmail.com>
2021-12-20 19:36:20 +0100

    added info about final price

--- projekt/js/ticketNumber.js
+++ projekt/js/ticketNumber.js
@@ -3,11 +3,17 @@ let counter = 0;
 function increaseTicketNumber() {
     counter ++;
     document.getElementById("ticketNumber").innerHTML = counter;
+    calculatePrice()
 }
 
 function decreaseTicketNumber(){
     if(counter > 0){
         counter--;
         document.getElementById("ticketNumber").innerHTML = counter;
+        calculatePrice()
     }
+}
+function calculatePrice(){
+    let price = parseInt(document.getElementById("poolPrice").innerText);
+    document.getElementById("finalPrice").innerText = price * counter;
 }
